6521a816fbd12343f985dbe402c505a63474ae3d
TanXuecheng <tanxuecheng@outlook.com>
2015-10-29 00:02:56 +0800

    简单一个布局

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
 'use strict';
 
 var React = require('react-native');
@@ -10,44 +6,210 @@ var {
   StyleSheet,
   Text,
   View,
-} = React;
+  ScrollView,
+  TouchableHighlight
+  //Animated
+  } = React;
+
+
+var BrandColors = {
+  Facebook: '#3b5998',
+  Twitter: '#55acee'
+};
+
+var { Icon, TabBarIOS, Spinner} = require('react-native-icons');
 
 var AppAboutTianYuanyuan = React.createClass({
-  render: function() {
+  getInitialState: function () {
+    return {
+      selectedTab: 'home',
+      notifCount: 0,
+      presses: 0,
+      //rotation: Animated.Value(0)
+    };
+  },
+  render: function () {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS
+        selectedTab={this.state.selectedTab}
+        tintColor={'#c1d82f'}
+        barTintColor={'#000000'}
+        styles={styles.tabBar}>
+        <TabBarIOS.Item
+          name="home"
+          iconName={'ion|ios-home-outline'}
+          title={'圆又圆'}
+          badgeValue={'3'}
+          iconSize={32}
+          accessibilityLabel="Home Tab"
+          selected={this.state.selectedTab === 'home'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'home',
+            });
+          }}>
+          {this._renderContent()}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          name="messages"
+          iconName={'ion|chatboxes'}
+          title={'来聊天'}
+          badgeValue={'999+'}          
+          iconSize={32}
+          accessibilityLabel="Messages Tab"
+          selected={this.state.selectedTab === 'messages'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'messages',
+            });
+          }}>
+          {this._renderContent()}
+        </TabBarIOS.Item>        
+        <TabBarIOS.Item
+          name="articles"
+          iconName={'ion|ios-paper-outline'}
+          title={'文青'}
+          iconSize={32}
+          accessibilityLabel="Articles Tab"
+          selected={this.state.selectedTab === 'articles'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'articles',
+            });
+          }}>
+          {this._renderContent()}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          name="gemes"
+          iconName={'ion|ios-game-controller-b'}
+          title={'虐'}
+          iconSize={32}
+          accessibilityLabel="Games Tab"
+          selected={this.state.selectedTab === 'games'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'games',
+            });
+          }}>
+          {this._renderContent()}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          name="settings"
+          iconName={'ion|ios-gear'}
+          title={'设置'}
+          iconSize={32}
+          accessibilityLabel="Settings Tab"
+          selected={this.state.selectedTab === 'settings'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'settings',
+            });
+          }}>
+          {this._renderContent()}
+        </TabBarIOS.Item>
+      </TabBarIOS>
+    );
+  },
+  _renderContent: function () {
+    return (
+      <ScrollView contentContainerStyle={styles.container}>        
+      </ScrollView>
     );
   }
 });
 
+
 var styles = StyleSheet.create({
+  tabBar: {
+    backgroundColor: '#dfdfdf',
+    flex: 1,
+    color: '#ff0000',
+    tintColor: '#877324'
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  topContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  header: {
+    flexDirection: 'column',
+    backgroundColor: 'transparent',
+    fontSize: 18,
+    marginTop: 20,
+    marginBottom: 10,
+    color: '#555555',
   },
+  beer: {
+    width: 70,
+    height: 70,
+    margin: 10
+  },
+  github: {
+    width: 70,
+    height: 70,
+    margin: 10
+  },
+  facebook: {
+    width: 70,
+    height: 70,
+    margin: 10
+  },
+  lightbulb: {
+    width: 70,
+    height: 70,
+    margin: 10
+  },
+  twitterOutline: {
+    flexDirection: 'column',
+    width: 70,
+    height: 70,
+    alignItems: 'center'
+  },
+  twitterIcon: {
+    flex: 1,
+    width: 40,
+    height: 40
+  },
+  signInWithTwitterButton: {
+    backgroundColor: BrandColors.Twitter,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: 210,
+    padding: 5,
+    borderRadius: 3
+  },
+  signInWithTwitterIcon: {
+    width: 28,
+    height: 28,
+    marginLeft: 5
+  },
+  signInText: {
+    color: 'white',
+    marginLeft: 5,
+    fontFamily: 'HelveticaNeue-Medium',
+    fontSize: 15
+  },
+  signInWithFacebookButton: {
+    backgroundColor: BrandColors.Facebook,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    width: 210,
+    padding: 5,
+    borderRadius: 3,
+    marginTop: 10
+  },
+  signInWithFacebookIcon: {
+    width: 28,
+    height: 28,
+    marginLeft: 5
+  }
 });
 
 AppRegistry.registerComponent('AppAboutTianYuanyuan', () => AppAboutTianYuanyuan);
